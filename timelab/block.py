0e9032fc370c8db04625ecdbac1aa579c0fc4357
ogabrielluiz <gabrielf.almeida90@gmail.com>
2021-10-15 19:00:32 -0300

    Small changes to block

--- timelab/block.py
+++ timelab/block.py
@@ -1,5 +1,6 @@
 import functools
 from collections import OrderedDict
+import numpy as np
 
 import pandas as pd
 
@@ -45,10 +46,10 @@ def from_array(values, index=None, assets=None, channels=None):
 
 
 def from_block(block, values=None, index=None, assets=None, channels=None):
-    values = values or block.values
-    assets = assets or block.assets
-    index = index or block.index
-    channels = channels or block.channels
+    values = values if values is not None else block.values
+    assets = assets if assets is not None else block.assets
+    index = index if index is not None else block.index
+    channels = channels if channels is not None else block.channels
     return from_array(values, index, assets, channels)
 
 
@@ -56,21 +57,37 @@ class TimeBlock(pd.DataFrame):
     def __init__(self, df, *args, **kwargs):
         super().__init__(df, *args, **kwargs)
 
+        if any(asset in self.channels for asset in self.assets):
+            raise ValueError("assets and channels must not have duplicated names")
+
+    def __eq__(self, other):
+        if not isinstance(other, (TimeBlock, pd.DataFrame)):
+            raise TypeError("'other' must be a TimeBlock or pandas DataFrame")
+        return np.all(np.nan_to_num(self.values, nan=0) == np.nan_to_num(other.values, nan=0))
+
     @property
     def _constructor(self):
         return TimeBlock
 
     @property
+    def start(self):
+        return self.index[0]
+
+    @property
+    def end(self):
+        return self.index[-1]
+
+    @property
     def assets(self):
         assets = [col[0] for col in self.columns]
         # OrderedDict to keep order
-        return list(OrderedDict.fromkeys(assets))
+        return pd.Series(tuple(OrderedDict.fromkeys(assets)))
 
     @property
     def channels(self):
         channels = [col[1] for col in self.columns]
         # OrderedDict to keep order
-        return list(OrderedDict.fromkeys(channels))
+        return pd.Series(list(OrderedDict.fromkeys(channels)))
 
     @rebuild
     def filter_assets(self, assets):
@@ -97,6 +114,11 @@ class TimeBlock(pd.DataFrame):
         filtered = filtered.filter_channels(channels)
         return filtered
 
+    def drop(self, assets=None, channels=None):
+        filtered = self.drop_assets(assets)
+        filtered = filtered.drop_channels(channels)
+        return filtered
+
     def drop_assets(self, assets):
         if isinstance(assets, str):
             assets = [assets]
@@ -109,32 +131,50 @@ class TimeBlock(pd.DataFrame):
         new_channels = [c for c in self.channels if c not in channels]
         return self.filter_channels(new_channels)
 
-    def rename_asset(self, value, new_value):
-        assets = replace(self.assets, value, new_value)
-        return from_block(self, assets=assets)
+    def rename_assets(self, values, new_values):
+        values = values if isinstance(values, list) else [values]
+        new_values = new_values if isinstance(new_values, list) else [new_values]
 
-    def rename_channel(self, value, new_value):
-        channels = replace(self.channels, value, new_value)
-        return from_block(self, channels=channels)
+        if len(values) != len(new_values):
+            raise ValueError("'values' must have the same length as 'new_values'")
 
-    def apply(self, func, on="timestamps"):
+        assets = self.assets.replace(to_replace=values, value=new_values)
+        return from_block(self, assets=assets.values)
 
-        if on == "timestamps":
+    def rename_channels(self, values, new_values):
+        values = values if isinstance(values, list) else [values]
+        new_values = new_values if isinstance(new_values, list) else [new_values]
+
+        if len(values) != len(new_values):
+            raise ValueError("'values' must have the same length as 'new_values'")
+
+        channels = self.channels.replace(to_replace=values, value=new_values)
+        return from_block(self, channels=channels.values)
+
+    def apply(self, func, axis=0):
+
+        if axis == 0:
             return self._timestamp_apply(func)
-        elif on == "channels":
+        elif axis == 1:
             return self._channel_apply(func)
 
-        raise ValueError(f"{on} not acceptable for 'on'. Available values are ['timestamps', 'channels']")
+        raise ValueError(f"{axis} not acceptable for 'axis'. Available values are [0, 1]")
 
     def _timestamp_apply(self, func):
         return self.pandas().apply(func, axis=0).to_frame().T
 
     def _channel_apply(self, func):
         splits = self.split_assets()
-        applied = [data.pandas().apply(func, axis=1).to_frame() for data in splits]
-        applied = [from_array(data.values, index=data.index) for data in applied]
-        applied = [data.rename_asset(0, asset) for data, asset in zip(applied, self.assets)]
-        return pd.concat(applied).rename_channel(0, "new_channel")
+        splits = [data.pandas().apply(func, axis=1).to_frame() for data in splits]
+        splits = [from_array(data.values, index=data.index) for data in splits]
+        splits = [data.rename_asset(0, asset) for data, asset in zip(splits, self.assets)]
+        return pd.concat(splits).rename_channel(0, "new_channel")
+
+    @rebuild
+    def add_channel(self, name, values):
+        for asset in self.assets:
+            self.loc[:, (asset, name)] = values
+        return self
 
     def split_assets(self):
         return [self.filter(asset) for asset in self.assets]
