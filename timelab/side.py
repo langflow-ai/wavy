19bcbf6e929e9b0e6208e2092cf87b18d3d27169
Rodrigo Nader <rodrigosilvanader@gmail.com>
2021-10-19 22:06:30 -0300

    add operation conditions

--- timelab/side.py
+++ timelab/side.py
@@ -28,31 +28,55 @@ class PanelSide:
         except AttributeError:
             raise AttributeError(f"'PanelSide' object has no attribute '{name}'")
 
-    def __abs__(self, other):
-        return PanelSide([block.__abs__(other) for block in self.blocks])
+    def __abs__(self):
+        return PanelSide([block.__abs__() for block in self.blocks])
 
     def __add__(self, other):
+        if isinstance(other, PanelSide):
+            return PanelSide([block.__add__(other_block) for block,
+            other_block in zip(self.blocks, other)])
         return PanelSide([block.__add__(other) for block in self.blocks])
 
     def __sub__(self, other):
+        if isinstance(other, PanelSide):
+            return PanelSide([block.__sub__(other_block) for block,
+            other_block in zip(self.blocks, other)])
         return PanelSide([block.__sub__(other) for block in self.blocks])
 
     def __mul__(self, other):
+        if isinstance(other, PanelSide):
+            return PanelSide([block.__mul__(other_block) for block,
+            other_block in zip(self.blocks, other)])
         return PanelSide([block.__mul__(other) for block in self.blocks])
 
     def __ge__(self, other):
+        if isinstance(other, PanelSide):
+            return PanelSide([block.__ge__(other_block) for block,
+            other_block in zip(self.blocks, other)])
         return PanelSide([block.__ge__(other) for block in self.blocks])
 
     def __gt__(self, other):
+        if isinstance(other, PanelSide):
+            return PanelSide([block.__gt__(other_block) for block,
+            other_block in zip(self.blocks, other)])
         return PanelSide([block.__gt__(other) for block in self.blocks])
 
     def __le__(self, other):
+        if isinstance(other, PanelSide):
+            return PanelSide([block.__le__(other_block) for block,
+            other_block in zip(self.blocks, other)])
         return PanelSide([block.__le__(other) for block in self.blocks])
 
     def __lt__(self, other):
+        if isinstance(other, PanelSide):
+            return PanelSide([block.__lt__(other_block) for block,
+            other_block in zip(self.blocks, other)])
         return PanelSide([block.__lt__(other) for block in self.blocks])
 
     def __pow__(self, other):
+        if isinstance(other, PanelSide):
+            return PanelSide([block.__pow__(other_block) for block,
+            other_block in zip(self.blocks, other)])
         return PanelSide([block.__pow__(other) for block in self.blocks])
 
     def __getitem__(self, key):
@@ -137,18 +161,3 @@ class PanelSide:
 
     def numpy(self):
         return np.array([block.numpy() for block in self.blocks])
-
-
-# def operate(func):
-#     def inner(self, other):
-#         return PanelSide([func(a, b) for a, b in zip(self.blocks, other.blocks)])
-#     return inner
-
-# for name in ['__add__','__mul__','__sub__']:
-#     setattr(PanelSide, name, operate(getattr(operator, name)))
-
-
-# def blockfunc(func):
-#     def inner(self):
-#         return PanelSide([getatrr(block, func.__name__)() for block in self.blocks])
-#     return inner
