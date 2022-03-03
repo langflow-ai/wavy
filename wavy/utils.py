56dce4dd1a0c9c324673836b19fe14d818dcfe7d
Rodrigo Nader <rodrigosilvanader@gmail.com>
2022-03-03 01:29:23 -0300

    clean utils

--- wavy/utils.py
+++ wavy/utils.py
@@ -5,180 +5,101 @@ import numpy as np
 import pandas as pd
 
 
-def add_level(df, level_name):
-    """
-    Add asset level to DataFrame
 
-    Args:
-        df (DataFrame): Pandas DataFrame
-        level_name (string): Asset level name
-
-    Returns:
-        ``DataFrame``: DataFrame with asset level added
-    """
-
-    # return pd.concat({level_name: df.T}, names=[level_name]).T
-    df.columns = pd.MultiIndex.from_product([[level_name], df.columns])
-    return df
+def reverse_pct_change(panel, df):
+    df = df.shift() * (1 + panel.as_dataframe())
+    return panel.update(df)
 
 
-def replace(ls, value, new_value):
-    ls = copy(ls)
-    idx = ls.index(value)
-    ls[idx] = new_value
-    return ls
+
+# def add_level(df, level_name):
+#     """
+#     Add asset level to DataFrame
+
+#     Args:
+#         df (DataFrame): Pandas DataFrame
+#         level_name (string): Asset level name
+
+#     Returns:
+#         ``DataFrame``: DataFrame with asset level added
+#     """
+
+#     # return pd.concat({level_name: df.T}, names=[level_name]).T
+#     df.columns = pd.MultiIndex.from_product([[level_name], df.columns])
+#     return df
 
 
-def ffill(arr, axis):
-    arr = arr.astype(float)
-    idx_shape = tuple([slice(None)] + [np.newaxis] * (len(arr.shape) - axis - 1))
-    idx = np.where(~np.isnan(arr), np.arange(arr.shape[axis])[idx_shape], 0)
-    np.maximum.accumulate(idx, axis=axis, out=idx)
-    slc = [
-        np.arange(k)[tuple(slice(None) if dim == i else np.newaxis for dim in range(len(arr.shape)))]
-        for i, k in enumerate(arr.shape)
-    ]
-
-    slc[axis] = idx
-    return arr[tuple(slc)]
+# def replace(ls, value, new_value):
+#     ls = copy(ls)
+#     idx = ls.index(value)
+#     ls[idx] = new_value
+#     return ls
 
 
-def bfill(arr, axis):
-    arr = arr.astype(float)
-    idx_shape = tuple([slice(None)] + [np.newaxis] * (len(arr.shape) - axis - 1))
-    idx = np.where(~np.isnan(arr), np.arange(arr.shape[axis])[idx_shape], arr.shape[axis] - 1)
-    slc = [
-        np.arange(k)[tuple(slice(None) if dim == i else np.newaxis for dim in range(len(arr.shape)))]
-        for i, k in enumerate(arr.shape)
-    ]
+# def ffill(arr, axis):
+#     arr = arr.astype(float)
+#     idx_shape = tuple([slice(None)] + [np.newaxis] * (len(arr.shape) - axis - 1))
+#     idx = np.where(~np.isnan(arr), np.arange(arr.shape[axis])[idx_shape], 0)
+#     np.maximum.accumulate(idx, axis=axis, out=idx)
+#     slc = [
+#         np.arange(k)[tuple(slice(None) if dim == i else np.newaxis for dim in range(len(arr.shape)))]
+#         for i, k in enumerate(arr.shape)
+#     ]
 
-    slc[axis] = idx
-    return arr[tuple(slc)]
-
-
-# TODO: Check how this works
-def get_all_unique(array):
-    all_ = list(array[0])
-    all_.extend(i[-1] for i in array[1:])
-    return np.array(all_)
+#     slc[axis] = idx
+#     return arr[tuple(slc)]
 
 
-def all_equal(iterable):
-    """
-    Check if all the iterables are equals.
+# def bfill(arr, axis):
+#     arr = arr.astype(float)
+#     idx_shape = tuple([slice(None)] + [np.newaxis] * (len(arr.shape) - axis - 1))
+#     idx = np.where(~np.isnan(arr), np.arange(arr.shape[axis])[idx_shape], arr.shape[axis] - 1)
+#     slc = [
+#         np.arange(k)[tuple(slice(None) if dim == i else np.newaxis for dim in range(len(arr.shape)))]
+#         for i, k in enumerate(arr.shape)
+#     ]
 
-    Parameters
-    ----------
-    iterable : TYPE
-        DESCRIPTION.
-
-    Returns
-    -------
-    TYPE
-        DESCRIPTION.
-
-    """
-    g = groupby(iterable)
-    return next(g, True) and not next(g, False)
+#     slc[axis] = idx
+#     return arr[tuple(slc)]
 
 
-def drop_zero_rows(df):
-    """
-    Drops rows with zero values.
+# def all_equal(iterable):
+#     """
+#     Check if all the iterables are equals.
 
-    """
-    df = df.replace(0, np.nan)
-    df = df.dropna(how="all")
-    df = df.fillna(0)
-    return df
+#     Parameters
+#     ----------
+#     iterable : TYPE
+#         DESCRIPTION.
 
+#     Returns
+#     -------
+#     TYPE
+#         DESCRIPTION.
 
-def entangle(a, b):
-    units = b.columns.levels[0].tolist()
-    channels = b.columns.levels[1].tolist()
-    a = a.filter(units, channels)
-    a = a.loc[b.index]
-    return a, b
-
-
-def revert_pct_change(original, changed, gap=0):
-    return (original.shift(gap) * changed) + original.shift(gap)
+#     """
+#     g = groupby(iterable)
+#     return next(g, True) and not next(g, False)
 
 
-def smash_array(array):
-    """
-    Transforms an 3D or 4D array into a D-1 dimension array.
-
-    Parameters
-    ----------
-    array : TYPE
-        DESCRIPTION.
-
-    Returns
-    -------
-    TYPE
-        DESCRIPTION.
-
-    """
-    if array.ndim == 4:
-        arr_list = []
-        for arr in array:
-            arr = np.hstack([arr[i] for i in range(len(arr))])
-            arr_list.append(arr)
-        return np.array(arr_list)
-
-    elif array.ndim == 3:
-        array = np.hstack([array[i] for i in range(len(array))])
-        return array
-    else:
-        raise ValueError("Array must have 3 or 4 dimensions")
+# def add_dim(x, n=1):
+#     for _ in range(n):
+#         x = np.array([x])
+#     return x
 
 
-def add_dim(x, n=1):
-    for _ in range(n):
-        x = np.array([x])
-    return x
+# # Rolling Functions
 
 
-def shift(array, n):
-    """
-    Equivalent to pandas shift.
-    """
-    shifted = np.full(array.shape, np.nan)
-    if n == 0:
-        shifted = copy(array)
-    elif n > 0:
-        shifted[n:] = array[:-n]
-    else:
-        shifted[:n] = array[-n:]
-    return shifted
+# def last_max(x):
+#     """
+#     Return True if last element is the biggest one
+#     """
+#     return x[-1] > np.max(x[:-1])
 
 
-# Rolling Functions
-
-
-def last_max(x):
-    """
-    Return True if last element is the biggest one
-    """
-    return x[-1] > np.max(x[:-1])
-
-
-def last_min(x):
-    """
-    Return True if last element is the smallest one
-    """
-    return x[-1] < np.min(x[:-1])
-
-
-def _get_active(obj):
-    return getattr(obj, f'_{obj._active_block}')
-
-
-def _get_block_attr(obj, name):
-    if obj._active_block == "x":
-        return getattr(obj._x, name)
-    elif obj._active_block == "y":
-        return getattr(obj._y, name)
-    if obj._active_block is None:
-        return (getattr(obj._x, name), getattr(obj._y, name))
+# def last_min(x):
+#     """
+#     Return True if last element is the smallest one
+#     """
+#     return x[-1] < np.min(x[:-1])
